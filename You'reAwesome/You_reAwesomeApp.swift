cf4ef772690dddf5f3995f82186d98e5ed84d2c3
eberbreton <eberbreton200@gmail.com>
2025-02-10 11:32:19 -0800

    Initial Commit

---
+++ You'reAwesome/You_reAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  You_reAwesomeApp.swift
+//  You'reAwesome
+//
+//  Created by Student1 on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct You_reAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

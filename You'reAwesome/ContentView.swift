77a6082f3e8746fd56d90c62fe4df2c33368bf0c
eberbreton <eberbreton200@gmail.com>
2025-03-11 10:54:21 -0700

    First Refactoring with Functions added

--- You'reAwesome/ContentView.swift
+++ You'reAwesome/ContentView.swift
@@ -8,88 +8,85 @@
 import SwiftUI
 import AVFAudio
 
-    struct ContentView: View {
-        @State private var message = ""
-        @State private var imageName = ""
-        @State private var lastMessageNumber = -1 // lastMessageNumber will never be -1
-        @State private var lastImageNumber = -1
-        @State private var lastSoundNumber = -1
-        @State private var audioPlayer :AVAudioPlayer!
-        let numberOfImages = 10 // images labeled image0 - image9
-        let numberOfSounds = 6 // sounds labled sound0 - sound5
+struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+    @State private var lastMessageNumber = -1 // lastMessageNumber will never be -1
+    @State private var lastImageNumber = -1
+    @State private var lastSoundNumber = -1
+    @State private var audioPlayer: AVAudioPlayer!
+    let numberOfImages = 10 // images labeled image0 - image9
+    let numberOfSounds = 6 // sounds labled sound0 - sound5
+    
+    var body: some View {
         
-        var body: some View {
+        VStack {
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
             
-            VStack{
-                Text(message)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .foregroundStyle(.red)
-                    .multilineTextAlignment(.center)
-                    .minimumScaleFactor(0.5)
-                    .frame(height: 100)
-                    .animation(.easeInOut(duration: 0.15), value: message)
-               
-                Spacer()
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+                .animation(.easeInOut(duration: 0.15), value: message)
+            
+            Spacer()
+            
+            Button("Show Message"){
+                let messages = ["You Are Awesome!",
+                                "You Are Great",
+                                "You Are Fantastic!",
+                                "Fabulous? That's You!",
+                                "You Make Me Smile!",
+                                "When the Genius Bar Needs Help, They Call You"]
                 
-                Image(imageName)
-                    .resizable()
-                    .scaledToFit()
-                    .clipShape(RoundedRectangle(cornerRadius: 30))
-                    .shadow(radius: 30)
-                    .animation(.easeInOut(duration: 0.15), value: message)
-              
-                Spacer()
+                lastMessageNumber = nonRepeatingRandom(lastNubmer: lastMessageNumber, upperBound: messages.count-1)
+                message = messages[lastMessageNumber]
                 
+                lastImageNumber = nonRepeatingRandom(lastNubmer: lastImageNumber, upperBound: numberOfImages-1)
+                imageName = "image\(lastImageNumber)"
                 
-                Button("Show Message"){
-                    let messages = ["You Are Awesome!",
-                                    "You Are Great",
-                                    "You Are Fantastic!",
-                                    "Fabulous? That's You!",
-                                    "You Make Me Smile!",
-                                    "When the Genius Bar Needs Help, They Call You"]
-
-                    var messageNumber : Int
-                    repeat {
-                        messageNumber = Int.random(in: 0...messages.count-1)
-                    }while messageNumber == lastMessageNumber
-                    message = messages[messageNumber]
-                    lastMessageNumber = messageNumber
-                    
-                    var imageNumber : Int
-                    repeat {
-                        imageNumber = Int.random(in: 0...(numberOfImages-1))
-                    } while imageNumber == lastImageNumber
-                    imageName = "image\(imageNumber)"
-                    lastImageNumber = imageNumber
-                    
-                    var soundNubmer : Int
-                    repeat {
-                        soundNubmer = Int.random(in: 0...(numberOfSounds-1))
-                    } while soundNubmer == lastSoundNumber
-                    lastSoundNumber = soundNubmer
-                    let soundName = "sound\(soundNubmer)"
-                    
-                    guard let soundFile = NSDataAsset(name: soundName) else {
-                        print("😡 Could not read file named\(soundName)")
-                        return
-                    }
-                    do {
-                        audioPlayer = try AVAudioPlayer(data: soundFile.data)
-                        audioPlayer.play()
-                    }catch{
-                        print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
-                        }
-                    }
-                .buttonStyle(.borderedProminent)
-                .font(.title2)
-       
-                }
-                .padding()
+                lastSoundNumber = nonRepeatingRandom(lastNubmer: lastSoundNumber, upperBound: numberOfSounds-1)
+                playSound(soundName:  "sound\(lastSoundNumber)")
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            
         }
+        .padding()
         
-        #Preview {
-            ContentView()
+    }
+    
+    func nonRepeatingRandom(lastNubmer: Int, upperBound: Int) -> Int {
+        var newNumber : Int
+        repeat{
+            newNumber = Int.random(in: 0...upperBound)
+        }while newNumber == lastNubmer
+        return newNumber
+    }
+    
+    func playSound (soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡 Could not read file named\(soundName)")
+            return
         }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
